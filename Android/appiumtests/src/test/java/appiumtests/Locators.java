d6b910ce01551fa0f3fd550f63cd7345a12d8c71
passant23 <passantahmedmaher@gmail.com>
2021-06-08 14:03:10 +0200

    Android and Web done

---
+++ Android/appiumtests/src/test/java/appiumtests/Locators.java
@@ -0,0 +1,11 @@
+package appiumtests;
+
+public class Locators {
+
+	//Sign Up Locators
+	String emailAddressField = "https://www.oracle.com/java/technologies/javase/codeconventions-namingconventions.html";
+	
+	
+	
+	
+}
